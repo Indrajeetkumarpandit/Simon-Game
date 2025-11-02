d9ce5bdd9059855a6949ee60acb45e968131b357
Indrajeet Kumar Prajapati <107981841+Indrajeetkumarpandit@users.noreply.github.com>
2025-11-02 14:40:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "blue", "purple", "yellow"];
+
+let started = false;
+
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+    if(started == false) {
+        console.log("Game Started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function Gameflash(btn) {
+    btn.classList.add("btnflash");
+    setTimeout(function () {
+        btn.classList.remove("btnflash");
+    }, 250);
+}
+
+function userflash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 4);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    Gameflash(randBtn);
+}
+
+function checkans(idx) {
+    if(userSeq[idx] === gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length) {
+         setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 100);
+    }
+}
+
+function btnPress() {
+    console.log(this);
+    let btn = this;
+    userflash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkans(userSeq.length-1);
+}
+
+let allbts = document.querySelectorAll(".btn");
+for(btn of allbts) {
+    btn.addEventListener("click", btnPress);
+
+}
